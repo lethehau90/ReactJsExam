70c9eb07a05216bc350d6857d00edc1f36e5ee1b
Hau Le <lethehau90.vn@gmail.com>
2017-08-19 23:55:57 +0700

    update example

---
+++ public/trangchu.js
@@ -0,0 +1,14 @@
+ReactDOM.render(
+                <div>
+                    <h1> Le Hau </h1>
+                    <h1 className="mauvang"> Le Hau </h1>
+                </div>
+                , document.getElementById("root"));
+
+ReactDOM.render(
+    <div>
+        <h1 className="mauvang">Khoa Hoc</h1>
+        <h1 className="mauvang">ReactJS</h1>
+    </div>
+    , document.getElementById("root2")
+);
