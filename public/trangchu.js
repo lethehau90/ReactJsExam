7217e8d3060de06848ecc2d3bf009f40137d2c70
Hau Le <lethehau90.vn@gmail.com>
2017-08-20 09:05:03 +0700

    update event

--- public/trangchu.js
+++ public/trangchu.js
@@ -8,17 +8,36 @@ var HauLe = React.createClass({
         alert(this.props.children);
     },
 
+    //getInitState
+    getInitialState: function(){
+        return { tongHocVien : 10}
+    },
+
+    themHocVien(){
+        this.setState({
+            tongHocVien: this.state.tongHocVien + 1
+        });
+    },
+     //end getInitState
+
     render : function(){
         return(
             <div>
                 <h1 className="mauvang">{this.props.ten} - {this.props.giangvien}</h1>
                 <p>{this.props.children}</p>
+
+                {/*setState*/}
+                <div>So hoc vien : {this.state.tongHocVien}</div> <hr/>
+
                 <button onClick={this.taskInfo}>Thong tin</button>
                 <p>
                     <button onClick={()=>{
                         var str = this.props.ten + ' giang vien: ' + this.props.giangvien;
                         getName(str);
                         }}>get name</button>
+                </p>
+                <p>
+                    <button onClick={this.themHocVien}>Them</button>
                 </p>
                 <KhoaHoc/>
             </div>
